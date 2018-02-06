ca3084679b8a740f2120f3a8a65409aca4a0f74c
bekasovoleg <o.bekasov96@gmail.com>
2018-02-06 22:03:46 +0300

    added full image view

--- views/images/show.php
+++ views/images/show.php
@@ -1,7 +1,13 @@
-<p>This is the requested image:</p>
+<h3>This is the requested image:</h3>
 
-<p><?php echo $image->id; ?></p>
-<p><?php echo $image->name; ?></p>
-<p><?php echo $image->alt; ?></p>
-<p><?php echo $image->title; ?></p>
-<p><?php echo $image->views; ?></p>
+<div class="image-box-full">
+  <div class="image-full">
+    <img src="<?php echo "assets/img/$image->name"; ?>"
+         alt="<?php echo "$image->alt"; ?>"
+         title="<?php echo "$image->title"; ?>">
+  </div>
+
+  <div class="full-image-link">
+   <?php echo "$image->name ($image->views views)"; ?>
+  </div>
+</div>
