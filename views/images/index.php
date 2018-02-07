9828c829fec0174a8c19eaf8c197751c3f92c67b
bekasovoleg <o.bekasov96@gmail.com>
2018-02-07 16:34:16 +0300

    ui improvement

--- views/images/index.php
+++ views/images/index.php
@@ -1,38 +1,40 @@
-<aside class="load-image">
-  <h3>Add new image:</h3>
+<section class="img-prew-list">
+  <aside class="load-image">
+    <h3>Add new image:</h3>
 
-  <form class="load-image-form" action="?controller=images&action=update" method="post" enctype="multipart/form-data">
+    <form class="load-image-form" action="?controller=images&action=update" method="post" enctype="multipart/form-data">
 
-      <label for="alt">Alt text:</label>
-      <input id="alt" type="text" name="alt" required><br>
+        <label for="alt">Alt text:</label>
+        <input id="alt" type="text" name="alt" required><br>
 
-      <label for="title">Title:</label>
-      <input id="title" type="text" name="title" required><br>
+        <label for="title">Title:</label>
+        <input id="title" type="text" name="title" required><br>
 
-      <input type="file" accept="image/*" name="file" />
+        <input type="file" accept="image/*" name="file" />
 
-      <input type="submit" value="Load file">
-  </form>
-</aside>
+        <input type="submit" value="Load file">
+    </form>
+  </aside>
 
-<section class="images">
-  <h3>Here is a list of all images:</h3>
+  <section class="images">
+    <h3>Here is a list of all images:</h3>
 
 
-  <div class="images-list">
-  <?php foreach($images as $image) { ?>
-    <div class="image-box">
+    <div class="images-list">
+    <?php foreach($images as $image) { ?>
+      <div class="image-box">
 
-      <div class="image-preview">
-        <img src="<?php echo "assets/img/min_$image->name"; ?>"
-             alt="<?php echo "$image->alt"; ?>"
-             title="<?php echo "$image->title"; ?>">
+        <div class="image-preview">
+          <img src="<?php echo "assets/img/min_$image->name"; ?>"
+               alt="<?php echo "$image->alt"; ?>"
+               title="<?php echo "$image->title"; ?>">
+        </div>
+
+        <div class="full-image-link">
+          <a href='?controller=images&action=show&id=<?php echo $image->id; ?>'><?php echo "$image->name ($image->views views)"; ?></a>
+        </div>
       </div>
-
-      <div class="full-image-link">
-        <a href='?controller=images&action=show&id=<?php echo $image->id; ?>'><?php echo "$image->name ($image->views views)"; ?></a>
-      </div>
+    <?php } ?>
     </div>
-  <?php } ?>
-  </div>
+  </section>
 </section>
